04392378641095191317bac65ff8305699a707cd
DuvanRozoP <rozoparraduvan@gmail.com>
2023-07-02 00:35:29 +0200

    feat: router for endpoints

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { Express } from 'express';
 import fs from 'fs';
 import path from 'path';
 import { messageLog, errorMessage } from './config';
-
 const directorio = path.join(__dirname, 'network');
 const Routers = async (server: Express) => {
   try {
@@ -14,17 +12,17 @@ const Routers = async (server: Express) => {
     if (files.length === 0) throw new Error('No se encuentra ningun Network');
 
     const filePromises = files.map(async (file) => {
-      if (file.endsWith('.ts')) {
+      if (file.endsWith('.ts') || file.endsWith('.js')) {
         const filePath = path.join(directorio, file);
         const fileName = path.parse(filePath).name;
-        const module = await import(filePath);
-        server.use(`/${fileName}`, module.default);
-        messageLog('check', `Endpoint: /${fileName} ✅`);
+        const { default: router } = await import(filePath);
+        server.use(`/${fileName}`, router);
+        messageLog('info', `Endpoint: /${fileName} ✅`);
       } else throw new Error(`El archivo ${file} no es valido`);
     });
 
     await Promise.all(filePromises).then(() =>
-      messageLog('check', `EndPoints Complete ✅`)
+      messageLog('info', `✅ EndPoints Complete ✅`)
     );
   } catch (error: unknown) {
     errorMessage(error);
